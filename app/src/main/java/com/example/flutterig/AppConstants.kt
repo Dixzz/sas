eb0b17484a9bb13ed1fc24fb5f7c64f0e13f3593
Satyam Sharma <satyam@tummoc.com>
2023-05-09 18:41:28 +0530

    init

---
+++ app/src/main/java/com/example/flutterig/AppConstants.kt
@@ -0,0 +1,5 @@
+package com.example.flutterig
+
+object FlutterMethodConstants {
+    const val INIT = "init"
+}
---
+++ app/src/main/java/com/example/flutterig/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.flutterig
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.flutter_test.EntryFlutterActivity
+import com.example.flutter_test.FlutterConfig
+import com.example.flutterig.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.gawd.setOnClickListener {
+            FlutterConfig.setKey("12345")
+            FlutterConfig.open(this)
+        }
+    }
+}
